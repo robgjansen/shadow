robgjansen/shadow

commit f5b955fc15cd26e9b1f313bb99cce34bb2a66095

Handle libc without preadv2, pwritev2, and/or statx

src/main/host/descriptor/file.c
@@ -14,6 +14,7 @@
 #include <sys/file.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
+#include <sys/syscall.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -294,7 +295,7 @@ ssize_t file_read(File* file, void* buf, size_t bufSize) {
     return (result < 0) ? -errno : result;
 }
 
-ssize_t file_pread(File* file, void* buf, size_t bufSize, off_t offset) {
+ssize_t file_preadv(File* file, void* buf, size_t bufSize, off_t offset) {
     MAGIC_ASSERT(file);
 
     if (!_file_getOSBackedFD(file)) {
@@ -311,6 +312,7 @@ ssize_t file_pread(File* file, void* buf, size_t bufSize, off_t offset) {
     return (result < 0) ? -errno : result;
 }
 
+#ifdef SYS_preadv2
 ssize_t file_preadv2(File* file, const struct iovec* iov, int iovcnt,
                      off_t offset, int flags) {
     MAGIC_ASSERT(file);
@@ -330,6 +332,7 @@ ssize_t file_preadv2(File* file, const struct iovec* iov, int iovcnt,
         preadv2(_file_getOSBackedFD(file), iov, iovcnt, offset, flags);
     return (result < 0) ? -errno : result;
 }
+#endif
 
 ssize_t file_write(File* file, const void* buf, size_t bufSize) {
     MAGIC_ASSERT(file);
@@ -348,7 +351,7 @@ ssize_t file_write(File* file, const void* buf, size_t bufSize) {
     return (result < 0) ? -errno : result;
 }
 
-ssize_t file_pwrite(File* file, const void* buf, size_t bufSize, off_t offset) {
+ssize_t file_pwritev(File* file, const void* buf, size_t bufSize, off_t offset) {
     MAGIC_ASSERT(file);
 
     if (!_file_getOSBackedFD(file)) {
@@ -365,6 +368,7 @@ ssize_t file_pwrite(File* file, const void* buf, size_t bufSize, off_t offset) {
     return (result < 0) ? -errno : result;
 }
 
+#ifdef SYS_pwritev2
 ssize_t file_pwritev2(File* file, const struct iovec* iov, int iovcnt,
                       off_t offset, int flags) {
     MAGIC_ASSERT(file);
@@ -384,6 +388,7 @@ ssize_t file_pwritev2(File* file, const struct iovec* iov, int iovcnt,
         pwritev2(_file_getOSBackedFD(file), iov, iovcnt, offset, flags);
     return (result < 0) ? -errno : result;
 }
+#endif
 
 int file_fstat(File* file, struct stat* statbuf) {
     MAGIC_ASSERT(file);
@@ -817,6 +822,7 @@ int file_renameat2(File* olddir, const char* oldpath, File* newdir,
     return (result < 0) ? -errno : result;
 }
 
+#ifdef SYS_statx
 int file_statx(File* dir, const char* pathname, int flags, unsigned int mask,
                struct statx* statxbuf) {
     debug("File %i statx os-backed file %i", dir ? _file_getFD(dir) : 0,
@@ -826,3 +832,4 @@ int file_statx(File* dir, const char* pathname, int flags, unsigned int mask,
         SYS_statx, _file_getOSBackedFD(dir), pathname, flags, mask, statxbuf);
     return (result < 0) ? -errno : result;
 }
+#endif

src/main/host/descriptor/file.h
@@ -9,6 +9,7 @@
 #include <stddef.h>
 #include <sys/stat.h>
 #include <sys/statfs.h>
+#include <sys/syscall.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/uio.h>
@@ -57,13 +58,17 @@ char* file_getAbsolutePath(File* file);
 // ****************************************
 
 ssize_t file_read(File* file, void* buf, size_t bufSize);
-ssize_t file_pread(File* file, void* buf, size_t bufSize, off_t offset);
+ssize_t file_preadv(File* file, void* buf, size_t bufSize, off_t offset);
+#ifdef SYS_preadv2
 ssize_t file_preadv2(File* file, const struct iovec* iov, int iovcnt,
                      off_t offset, int flags);
+#endif
 ssize_t file_write(File* file, const void* buf, size_t bufSize);
-ssize_t file_pwrite(File* file, const void* buf, size_t bufSize, off_t offset);
+ssize_t file_pwritev(File* file, const void* buf, size_t bufSize, off_t offset);
+#ifdef SYS_pwritev2
 ssize_t file_pwritev2(File* file, const struct iovec* iov, int iovcnt,
                       off_t offset, int flags);
+#endif
 int file_fstat(File* file, struct stat* statbuf);
 int file_fstatfs(File* file, struct statfs* statbuf);
 int file_fsync(File* file);
@@ -113,7 +118,9 @@ ssize_t file_readlinkat(File* dir, const char* pathname, char* buf,
                         size_t bufsize);
 int file_renameat2(File* olddir, const char* oldpath, File* newdir,
                    const char* newpath, unsigned int flags);
+#ifdef SYS_statx
 int file_statx(File* dir, const char* pathname, int flags, unsigned int mask,
                struct statx* statxbuf);
+#endif
 
 #endif /* SRC_MAIN_HOST_DESCRIPTOR_FILE_H_ */

src/main/host/syscall/unistd.c
@@ -130,7 +130,7 @@ static SysCallReturn _syscallhandler_readHelper(SysCallHandler* sys, int fd,
             if (offset == 0) {
                 result = file_read((File*)desc, buf, sizeNeeded);
             } else {
-                result = file_pread((File*)desc, buf, sizeNeeded, offset);
+                result = file_preadv((File*)desc, buf, sizeNeeded, offset);
             }
             break;
         case DT_TIMER:
@@ -229,7 +229,7 @@ static SysCallReturn _syscallhandler_writeHelper(SysCallHandler* sys, int fd,
             if (offset == 0) {
                 result = file_write((File*)desc, buf, sizeNeeded);
             } else {
-                result = file_pwrite((File*)desc, buf, sizeNeeded, offset);
+                result = file_pwritev((File*)desc, buf, sizeNeeded, offset);
             }
             break;
         case DT_TIMER: result = -EINVAL; break;

src/main/host/syscall/fcntl.c
@@ -32,7 +32,10 @@ static int _syscallhandler_fcntlHelper(SysCallHandler* sys, File* file, int fd,
         case F_GETSIG:
         case F_GETLEASE:
         case F_GETPIPE_SZ:
-        case F_GET_SEALS: {
+#ifdef F_GET_SEALS
+        case F_GET_SEALS:
+#endif
+        {
             // arg is ignored
             result = file_fcntl(file, command, NULL);
             break;
@@ -45,14 +48,20 @@ static int _syscallhandler_fcntlHelper(SysCallHandler* sys, File* file, int fd,
         case F_SETLEASE:
         case F_NOTIFY:
         case F_SETPIPE_SZ:
-        case F_ADD_SEALS: {
+#ifdef F_ADD_SEALS
+        case F_ADD_SEALS:
+#endif
+        {
             // arg is an int (we cast to void* here to appease the fcntl api)
             result = file_fcntl(file, command, (void*)argReg.as_i64);
             break;
         }
 
         case F_GETLK:
-        case F_OFD_GETLK: {
+#ifdef F_OFD_GETLK
+        case F_OFD_GETLK:
+#endif
+        {
             struct flock* flk_in = thread_newClonedPtr(
                 sys->thread, argReg.as_ptr, sizeof(*flk_in));
             result = file_fcntl(file, command, (void*)flk_in);
@@ -66,9 +75,14 @@ static int _syscallhandler_fcntlHelper(SysCallHandler* sys, File* file, int fd,
         }
 
         case F_SETLK:
+#ifdef F_OFD_SETLK
         case F_OFD_SETLK:
+#endif
         case F_SETLKW:
-        case F_OFD_SETLKW: {
+#ifdef F_OFD_SETLKW
+        case F_OFD_SETLKW:
+#endif
+        {
             const struct flock* flk =
                 thread_getReadablePtr(sys->thread, argReg.as_ptr, sizeof(*flk));
             result = file_fcntl(file, command, (void*)flk);
@@ -89,16 +103,26 @@ static int _syscallhandler_fcntlHelper(SysCallHandler* sys, File* file, int fd,
             break;
         }
 
+#ifdef F_GET_RW_HINT
         case F_GET_RW_HINT:
-        case F_GET_FILE_RW_HINT: {
+#endif
+#ifdef F_GET_FILE_RW_HINT
+        case F_GET_FILE_RW_HINT:
+#endif
+        {
             uint64_t* hint = thread_getWriteablePtr(
                 sys->thread, argReg.as_ptr, sizeof(*hint));
             result = file_fcntl(file, command, hint);
             break;
         }
 
+#ifdef F_SET_RW_HINT
         case F_SET_RW_HINT:
-        case F_SET_FILE_RW_HINT: {
+#endif
+#ifdef F_SET_FILE_RW_HINT
+        case F_SET_FILE_RW_HINT:
+#endif
+        {
             const uint64_t* hint = thread_getReadablePtr(
                 sys->thread, argReg.as_ptr, sizeof(*hint));
             result = file_fcntl(file, command, (void*)hint);

src/main/host/syscall/fileat.c
@@ -7,6 +7,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <sys/syscall.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -483,6 +484,7 @@ SysCallReturn syscallhandler_renameat2(SysCallHandler* sys,
         args->args[3].as_ptr, args->args[4].as_u64);
 }
 
+#ifdef SYS_statx
 SysCallReturn syscallhandler_statx(SysCallHandler* sys,
                                    const SysCallArgs* args) {
     int dirfd = args->args[0].as_i64;
@@ -517,3 +519,4 @@ SysCallReturn syscallhandler_statx(SysCallHandler* sys,
         .state = SYSCALL_DONE,
         .retval.as_i64 = file_statx(dir_desc, pathname, flags, mask, statxbuf)};
 }
+#endif

src/main/host/syscall/timerfd.c
@@ -104,6 +104,9 @@ SysCallReturn syscallhandler_timerfd_settime(SysCallHandler* sys,
     }
 
     /* Check for valid flags. */
+#ifndef TFD_TIMER_CANCEL_ON_SET
+#define TFD_TIMER_CANCEL_ON_SET 0
+#endif
     if (flags & ~(TFD_TIMER_ABSTIME | TFD_TIMER_CANCEL_ON_SET)) {
         return (SysCallReturn){.state = SYSCALL_DONE, .retval.as_i64 = -EINVAL};
     }

src/main/host/syscall/uio.c
@@ -6,6 +6,7 @@
 #include "main/host/syscall/uio.h"
 
 #include <errno.h>
+#include <sys/syscall.h>
 #include <sys/uio.h>
 
 #include "main/host/descriptor/descriptor.h"
@@ -116,7 +117,14 @@ _syscallhandler_readvHelper(SysCallHandler* sys, int fd, PluginPtr iovPtr,
             buffersv[i].iov_len = bufSize;
         }
 
+#ifdef SYS_preadv2
         result = file_preadv2((File*)desc, buffersv, iovlen, offset, flags);
+#else
+        if (flags) {
+            warning("Ignoring flags");
+        }
+        result = file_preadv((File*)desc, buffersv, iovlen, offset);
+#endif
 
         free(buffersv);
     } else {
@@ -229,7 +237,14 @@ _syscallhandler_writevHelper(SysCallHandler* sys, int fd, PluginPtr iovPtr,
             buffersv[i].iov_len = bufSize;
         }
 
+#ifdef SYS_pwritev2
         result = file_pwritev2((File*)desc, buffersv, iovlen, offset, flags);
+#else
+        if (flags) {
+            warning("Ignoring flags");
+        }
+        result = file_pwritev((File*)desc, buffersv, iovlen, offset);
+#endif
 
         free(buffersv);
     } else {

src/main/host/syscall_handler.c
@@ -209,10 +209,14 @@ SysCallReturn syscallhandler_make_syscall(SysCallHandler* sys,
         HANDLE(pipe2);
         HANDLE(pread64);
         HANDLE(preadv);
+#ifdef SYS_preadv2
         HANDLE(preadv2);
+#endif
         HANDLE(pwrite64);
         HANDLE(pwritev);
+#ifdef SYS_pwritev2
         HANDLE(pwritev2);
+#endif
         HANDLE(read);
         HANDLE(readahead);
         HANDLE(readlinkat);
@@ -224,7 +228,9 @@ SysCallReturn syscallhandler_make_syscall(SysCallHandler* sys,
         HANDLE(setsockopt);
         HANDLE(shutdown);
         HANDLE(socket);
+#ifdef SYS_statx
         HANDLE(statx);
+#endif
         HANDLE(symlinkat);
         HANDLE(sync_file_range);
         HANDLE(syncfs);

commit c32c00caa2cdfff73041ebb88587479b51d592ee

Correctly implement iovec versions of read/write
A regression was introduced in
f5b955fc15cd26e9b1f313bb99cce34bb2a66095
where we started handling preadv calls by the plugin with an OS
call to pread. We didn't notice the error because the tests that
use the iovec versions of read and write have not yet been
enabled.

This commit fixes it so that we are handling pread, pwrite, preadv,
and pwritev calls with the call to the corresponding OS function.

src/main/host/descriptor/file.c
@@ -295,7 +295,7 @@ ssize_t file_read(File* file, void* buf, size_t bufSize) {
     return (result < 0) ? -errno : result;
 }
 
-ssize_t file_preadv(File* file, void* buf, size_t bufSize, off_t offset) {
+ssize_t file_pread(File* file, void* buf, size_t bufSize, off_t offset) {
     MAGIC_ASSERT(file);
 
     if (!_file_getOSBackedFD(file)) {
@@ -309,6 +309,24 @@ ssize_t file_preadv(File* file, void* buf, size_t bufSize, off_t offset) {
     /* TODO: this may block the shadow thread until we properly handle
      * os-backed files in non-blocking mode. */
     ssize_t result = pread(_file_getOSBackedFD(file), buf, bufSize, offset);
+    return (result < 0) ? -errno : result;
+}
+
+ssize_t file_preadv(File* file, const struct iovec* iov, int iovcnt, off_t offset) {
+    MAGIC_ASSERT(file);
+
+    if (!_file_getOSBackedFD(file)) {
+        return -EBADF;
+    }
+
+    debug("File %i will preadv %d vector items from os-backed file %i at path "
+          "'%s'",
+          _file_getFD(file), iovcnt, _file_getOSBackedFD(file),
+          file->osfile.abspath);
+
+    /* TODO: this may block the shadow thread until we properly handle
+     * os-backed files in non-blocking mode. */
+    ssize_t result = preadv(_file_getOSBackedFD(file), iov, iovcnt, offset);
     return (result < 0) ? -errno : result;
 }
 
@@ -351,7 +369,7 @@ ssize_t file_write(File* file, const void* buf, size_t bufSize) {
     return (result < 0) ? -errno : result;
 }
 
-ssize_t file_pwritev(File* file, const void* buf, size_t bufSize, off_t offset) {
+ssize_t file_pwrite(File* file, const void* buf, size_t bufSize, off_t offset) {
     MAGIC_ASSERT(file);
 
     if (!_file_getOSBackedFD(file)) {
@@ -365,6 +383,24 @@ ssize_t file_pwritev(File* file, const void* buf, size_t bufSize, off_t offset) 
     /* TODO: this may block the shadow thread until we properly handle
      * os-backed files in non-blocking mode. */
     ssize_t result = pwrite(_file_getOSBackedFD(file), buf, bufSize, offset);
+    return (result < 0) ? -errno : result;
+}
+
+ssize_t file_pwritev(File* file, const struct iovec* iov, int iovcnt, off_t offset) {
+    MAGIC_ASSERT(file);
+
+    if (!_file_getOSBackedFD(file)) {
+        return -EBADF;
+    }
+
+    debug("File %i will pwritev %d vector items from os-backed file %i at "
+          "path '%s'",
+          _file_getFD(file), iovcnt, _file_getOSBackedFD(file),
+          file->osfile.abspath);
+
+    /* TODO: this may block the shadow thread until we properly handle
+     * os-backed files in non-blocking mode. */
+    ssize_t result = pwritev(_file_getOSBackedFD(file), iov, iovcnt, offset);
     return (result < 0) ? -errno : result;
 }
 

src/main/host/descriptor/file.h
@@ -58,13 +58,15 @@ char* file_getAbsolutePath(File* file);
 // ****************************************
 
 ssize_t file_read(File* file, void* buf, size_t bufSize);
-ssize_t file_preadv(File* file, void* buf, size_t bufSize, off_t offset);
+ssize_t file_pread(File* file, void* buf, size_t bufSize, off_t offset);
+ssize_t file_preadv(File* file, const struct iovec* iov, int iovcnt, off_t offset);
 #ifdef SYS_preadv2
 ssize_t file_preadv2(File* file, const struct iovec* iov, int iovcnt,
                      off_t offset, int flags);
 #endif
 ssize_t file_write(File* file, const void* buf, size_t bufSize);
-ssize_t file_pwritev(File* file, const void* buf, size_t bufSize, off_t offset);
+ssize_t file_pwrite(File* file, const void* buf, size_t bufSize, off_t offset);
+ssize_t file_pwritev(File* file, const struct iovec* iov, int iovcnt, off_t offset);
 #ifdef SYS_pwritev2
 ssize_t file_pwritev2(File* file, const struct iovec* iov, int iovcnt,
                       off_t offset, int flags);

src/main/host/syscall/unistd.c
@@ -131,7 +131,7 @@ static SysCallReturn _syscallhandler_readHelper(SysCallHandler* sys, int fd,
             if (offset == 0) {
                 result = file_read((File*)desc, buf, sizeNeeded);
             } else {
-                result = file_preadv((File*)desc, buf, sizeNeeded, offset);
+                result = file_pread((File*)desc, buf, sizeNeeded, offset);
             }
             break;
         case DT_TIMER:
@@ -230,7 +230,7 @@ static SysCallReturn _syscallhandler_writeHelper(SysCallHandler* sys, int fd,
             if (offset == 0) {
                 result = file_write((File*)desc, buf, sizeNeeded);
             } else {
-                result = file_pwritev((File*)desc, buf, sizeNeeded, offset);
+                result = file_pwrite((File*)desc, buf, sizeNeeded, offset);
             }
             break;
         case DT_TIMER: result = -EINVAL; break;